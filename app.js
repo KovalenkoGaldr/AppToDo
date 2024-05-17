9ce7639bebece6e90744ac70228906fb39097b92
Evgeny Kovalenko <gransmastas@gmail.com>
2024-05-17 18:15:49 +0300

    add functionality for all buttons

--- app.js
+++ app.js
@@ -2,8 +2,73 @@ const taskInput = document.getElementById('input-text');
 const form = document.getElementById('form');
 const tasksList = document.querySelector('#list');
 const emptyList = document.getElementById('emptyList')
+const btnAll = document.getElementById('all')
+const btnUnfulfilled = document.getElementById('unfulfilled')
+const btnCompleted = document.getElementById('completed')
+
+// отметить все задачи
+btnAll.addEventListener('click', () => {
+  markAllTasks();
+});
+
+function markAllTasks() {
+  const checkboxes = list.querySelectorAll('.group-checkbox');
+  checkboxes.forEach(checkbox => {
+      checkbox.checked = true;
+  });
+}
+
+// Удалить невыполненные задачи
+  btnUnfulfilled.addEventListener('click', () => {
+    removeUnfulfilledTasks();
+});
+
+function removeUnfulfilledTasks() {
+  const tasksUnfulfilled = list.querySelectorAll('.list-group-item');
+  tasksUnfulfilled.forEach(task => {
+      const checkbox = task.querySelector('.group-checkbox');
+      if (!checkbox.checked) {
+          task.remove();
+      }
+  });
+}
+
+// удалить выполненные задачи
+btnCompleted.addEventListener('click', () => {
+  removeCompletedTasks();
+});
+
+function removeCompletedTasks() {
+  const tasks = list.querySelectorAll('.list-group-item');
+  tasks.forEach(task => {
+      const checkbox = task.querySelector('.group-checkbox');
+      if (checkbox.checked) {
+          task.remove();
+      }
+  });
+}
+
 
 let tasks = [];
+
+if (localStorage.getItem(tasks)) {
+  tasks = JSON.parse(localStorage.getItem(tasks));
+}
+
+tasks.forEach(function (task){
+  const cssClass = task.done ? 'task-title line-through' : 'task-title'
+  
+  const taskHTML = `<li class="list-group-item" id="${task.id}">
+  <input class="group-checkbox" id="${id}-input" type="checkbox" onchange="toggle(${id})">
+  <button class="list-btn-group" data-action="delete" id="btn-group-del">
+      <img class="group-trash-btn" src="./assets/trash.svg" alt="корзина">
+  </button>
+  <span class="${cssClass}" id="${id}-span">${task.text}</span>
+  </li>`
+  
+  
+  tasksList.insertAdjacentHTML('afterbegin', taskHTML);
+})
 
 // редактирование списка
 
@@ -61,6 +126,8 @@ function addTask (event) {
 
   tasks.push(newTask)
 
+  saveToLocalStorage()
+
   const cssClass = newTask.done ? 'task-title line-through' : 'task-title'
   
   const taskHTML = `<li class="list-group-item" id="${newTask.id}">
@@ -89,12 +156,15 @@ function deleteTask(event) {
   const parenNode = event.target.closest('.list-group-item');
 
   const id = Number(parenNode.id)
-  const index = tasks.findIndex((task) => task.id === id)
-  tasks.splice(index, 1)
-
+  // const index = tasks.findIndex((task) => task.id === id)
+  // // удаляем задачу из массива с задачами
+  // tasks.splice(index, 1)
+  // удаляем задачу через фильтрацию массива
   tasks = tasks.filter((task) => task.id !== id)
-  console.log(tasks)
 
+  saveToLocalStorage()
+
+  // удаляем задачу из разметки
   parenNode.remove()
   }
 
@@ -109,4 +179,9 @@ function removeListItems() {
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
+  saveToLocalStorage()
 }
+
+function saveToLocalStorage() {
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+} 
